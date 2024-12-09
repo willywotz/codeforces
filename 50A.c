555a16d174ec0afd46b143d9b4a29bfc86b2c45a
willywotz <dogajack@gmail.com>
2024-12-09 22:35:14 +0700

    add 50A - Domino piling

---
+++ 50A.c
@@ -0,0 +1,11 @@
+// https://codeforces.com/problemset/problem/50/A
+
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &n, &m);
+    printf("%d\n", (m*n)/2);
+
+    return 0;
+}
